b4e3403fceff14ecf3bd5e2181144138238d4145
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-08-11 11:25:46 +0900

    Create install_opencv.sh

---
+++ install_opencv.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# wget
+
+# setting ###################################################
+
+OPENCV_VERSION="4-5-3" ;
+
+#############################################################
+
+sudo echo ;
+
+echo "===========================";
+echo " check swap size";
+echo "===========================";
+SWAPLIMIT=`free -m | egrep '^Swap:' | tr -s ' ' | awk '{print $2}'` ;
+
+echo $SWAPLIMIT;
+
+if $SWAPLIMIT < 6000 ; then
+
+  echo "not enough swap size";
+  echo "auto create and reboot";
+  echo "reboot finished. please retry";
+
+  echo "===========================";
+  echo " create expand swap size";
+  echo "===========================";
+  cd ;
+  git clone https://github.com/JetsonHacksNano/installSwapfile ;
+  cd installSwapfile ;
+  ./installSwapfile.sh ;
+  sudo reboot now ;
+
+fi
+
+echo "swap size is ok.";
+
+echo "===========================";
+echo " Install OpenCV Ver. ${OPENCV_VERSION}";
+echo "===========================";
+wget https://github.com/Qengineering/Install-OpenCV-Jetson-Nano/raw/main/OpenCV-${OPENCV_VERSION}.sh ;
+sudo chmod 755 ./OpenCV-${OPENCV_VERSION}.sh ;
+./OpenCV-${OPENCV_VERSION}.sh ;
+
+echo "fisnished.";
+echo "please try example programs =>";
+
+# todo
