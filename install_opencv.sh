25c41c668161dfe1b32ccdec17cdfb1e2ff7d7ac
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-02 07:55:18 +0900

    Update install_opencv.sh

--- install_opencv.sh
+++ install_opencv.sh
@@ -2,48 +2,37 @@
 
 # wget https://raw.githubusercontent.com/mugimugi555/jetsonnano/main/install_opencv.sh && bash install_opencv.sh ;
 
-# setting ###################################################
+#-----------------------------------------------------------------------------------------------------------------------
+# config
+#-----------------------------------------------------------------------------------------------------------------------
+OPENCV_VERSION="4-5-5" ;
 
-OPENCV_VERSION="4-5-3" ;
-
-#############################################################
-
+#-----------------------------------------------------------------------------------------------------------------------
+# check swap size";
+#-----------------------------------------------------------------------------------------------------------------------
 sudo echo ;
-
-echo "===========================";
-echo " check swap size";
-echo "===========================";
 SWAPLIMIT=`free -m | egrep '^Swap:' | tr -s ' ' | awk '{print $2}'` ;
-
 echo $SWAPLIMIT;
 
 if [ $SWAPLIMIT -lt 6000 ] ; then
 
-  echo "not enough swap size";
-  echo "auto create and reboot";
-  echo "reboot finished. please retry";
-
-  echo "===========================";
-  echo " create expand swap size";
-  echo "===========================";
   cd ;
   git clone https://github.com/JetsonHacksNano/installSwapfile ;
   cd installSwapfile ;
   ./installSwapfile.sh ;
-  sudo reboot now ;
+  # sudo reboot now ;
 
 fi
 
-echo "swap size is ok.";
-
-echo "===========================";
-echo " Install OpenCV Ver. ${OPENCV_VERSION}";
-echo "===========================";
-wget https://github.com/Qengineering/Install-OpenCV-Jetson-Nano/raw/main/OpenCV-${OPENCV_VERSION}.sh ;
+#-----------------------------------------------------------------------------------------------------------------------
+# install opencv
+#-----------------------------------------------------------------------------------------------------------------------
+wget hraw/main/OpenCV-${OPENCV_VERSION}.sh ;
 sudo chmod 755 ./OpenCV-${OPENCV_VERSION}.sh ;
 ./OpenCV-${OPENCV_VERSION}.sh ;
 
-echo "fisnished.";
-echo "please try example programs =>";
+#-----------------------------------------------------------------------------------------------------------------------
+# finish
+#-----------------------------------------------------------------------------------------------------------------------
 
-# todo
+# do examples
