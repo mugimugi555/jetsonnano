605a9bac5e2dcd3f9717654a53ea7dc615a3e150
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-02 12:55:31 +0900

    Update install_tensorflow.sh

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -4,38 +4,38 @@
 
 #-----------------------------------------------------------------------------------------------------------------------
 # note
-# for jetson nano and python 3.6 ( cp36 aarch64 )
+# https://docs.nvidia.com/deeplearning/frameworks/install-tf-jetson-platform/index.html
 # https://github.com/PINTO0309/Tensorflow-bin/tree/main/previous_versions
 # https://developer.download.nvidia.com/compute/redist/jp/v461/tensorflow/
 #-----------------------------------------------------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------------------------------------------------
-# download install shell
-#-----------------------------------------------------------------------------------------------------------------------
-cd ;
-wget https://developer.download.nvidia.com/compute/redist/jp/v461/tensorflow/tensorflow-2.7.0+nv22.1-cp36-cp36m-linux_aarch64.whl ;
-wget https://developer.download.nvidia.com/compute/redist/jp/v461/pytorch/torch-1.11.0a0+17540c5-cp36-cp36m-linux_aarch64.whl ;
+JETSON_VERSION=461 ;
 
 #-----------------------------------------------------------------------------------------------------------------------
-# install tensorflow 2.5 about 30 minutes
+# install library
 #-----------------------------------------------------------------------------------------------------------------------
-#sudo apt install -y libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev ;
-sudo apt install -y \
-  libhdf5-serial-dev hdf5-tools libhdf5-dev \
-  zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran ;
-pip cache purge ;
-/usr/bin/python3 -m pip install --upgrade pip ;
-#python3 -m pip install h5py
-python3 -m pip install \
-  tensorflow-hub \
-  tensorflow-datasets \
-  tensorflow-2.7.0+nv22.1-cp36-cp36m-linux_aarch64.whl \
-  torch-1.11.0a0+17540c5-cp36-cp36m-linux_aarch64.whl ;
+sudo apt update ;
+sudo apt install -y libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran ;
+
+#-----------------------------------------------------------------------------------------------------------------------
+# install python library
+#-----------------------------------------------------------------------------------------------------------------------
+sudo apt install -y python3-pip ;
+sudo pip3 install -U pip testresources setuptools==49.6.0 ; 
+sudo pip3 install -U --no-deps numpy==1.19.4 future==0.18.2 mock==3.0.5 keras_preprocessing==1.1.2 keras_applications==1.0.8 gast==0.4.0 protobuf pybind11 cython pkgconfig ;
+
+#-----------------------------------------------------------------------------------------------------------------------
+# install no-build-isolation h5py==3.1.0
+#-----------------------------------------------------------------------------------------------------------------------
+sudo env H5PY_SETUP_REQUIRES=0 pip3 install -U --no-build-isolation h5py==3.1.0
+
+#-----------------------------------------------------------------------------------------------------------------------
+# install tensorflow
+#-----------------------------------------------------------------------------------------------------------------------
+sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v$JETSON_VERSION tensorflow ;
+sudo pip3 install tensorflow-hub tensorflow-datasets ;
 
 #-----------------------------------------------------------------------------------------------------------------------
 # check install tensorflow
 #-----------------------------------------------------------------------------------------------------------------------
-python3 -c 'import tensorflow as tf; print(tf.__version__)'  # for Python 3
-
-#pip uninstall numpy
-#pip install numpy
+python3 -c 'import tensorflow as tf; print(tf.__version__)' ;
