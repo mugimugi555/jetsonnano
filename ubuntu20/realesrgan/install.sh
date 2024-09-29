a928c07c137dc1929a37f5b1745823b98ea3269a
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2024-09-29 18:12:34 +0900

    Update install.sh

--- ubuntu20/realesrgan/install.sh
+++ ubuntu20/realesrgan/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-#
-cd
+# ====== セクション1: 必要な依存パッケージのインストール ======
+echo "=== 依存パッケージをインストール中 ==="
+cd ~
 sudo apt install -y cmake git g++ libvulkan-dev vulkan-utils libopencv-dev
+
+# ====== セクション2: NCNNのクローンとビルド ======
+echo "=== NCNNをクローンしてビルド ==="
 git clone https://github.com/Tencent/ncnn.git
 cd ncnn
 git submodule update --init --recursive
@@ -12,8 +16,9 @@ cmake -D CMAKE_BUILD_TYPE=Release -D NCNN_VULKAN=ON ..
 make -j$(nproc)
 sudo make install
 
-#
-cd
+# ====== セクション3: CMakeのアップデート ======
+echo "=== CMakeのバージョンをアップデート ==="
+cd ~
 cmake --version
 sudo apt install -y build-essential libssl-dev
 wget https://github.com/Kitware/CMake/releases/download/v3.30.4/cmake-3.30.4-Linux-aarch64.tar.gz
@@ -22,28 +27,30 @@ cd cmake-3.30.4-linux-aarch64/
 sudo cp -r * /usr/local/
 cmake --version
 
-#
-cd
+# ====== セクション4: SSHキーの作成と設定 ======
+echo "=== SSHキーの作成 ==="
+cd ~
 ssh-keygen -t rsa -b 4096
 echo "========================================="
-echo "SSH keys -> new SSH key -> copy next code -> Add SSH key";
+echo "SSH keys -> new SSH key -> copy next code -> Add SSH key"
 echo "========================================="
 cat ~/.ssh/id_rsa.pub
 echo "========================================="
 xdg-open https://github.com/settings/keys
 
-#
-cd
+# ====== セクション5: Real-ESRGAN-ncnn-vulkanのクローンとビルド ======
+echo "=== Real-ESRGAN-ncnn-vulkanをクローンしてビルド ==="
+cd ~
 sudo apt-get install -y glslang-tools
 git clone https://github.com/xinntao/Real-ESRGAN-ncnn-vulkan.git --recursive
-cd Real-ESRGAN-ncnn-vulkan/
-cd src/
+cd Real-ESRGAN-ncnn-vulkan/src/
 mkdir build
 cd build
 cmake ..
 make -j$(nproc)
 
-# ベースURL
+# ====== セクション6: モデルファイルのダウンロード ======
+echo "=== モデルファイルをダウンロード中 ==="
 base_url="https://github.com/TheMurusTeam/FreeScaler/raw/refs/heads/main/FreeScaler/realesrgan/models/"
 
 # ダウンロードするファイルのリスト
@@ -66,11 +73,14 @@ output_dir="models"
 # 保存先ディレクトリが存在しない場合は作成
 mkdir -p "$output_dir"
 
-# for ループでファイルをダウンロード
+# forループでファイルをダウンロード
 for file in "${files[@]}"; do
     wget -P "$output_dir" "${base_url}${file}"
 done
 
-#
+# ====== セクション7: Real-ESRGANの実行 ======
+echo "=== 画像の拡大処理を実行中 ==="
 wget 'https://github.com/xinntao/Real-ESRGAN-ncnn-vulkan/blob/master/images/input2.jpg?raw=true' -O input.jpg
 ./realesrgan-ncnn-vulkan -i input -o output.jpg -s 2 -m models -n realesrgan-x4plus
+
+echo "=== 完了しました ==="
