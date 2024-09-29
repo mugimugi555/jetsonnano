3d1bdd162ccbed3e48733c4b1758d7fcc3938889
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2024-09-29 18:25:17 +0900

    Update install.sh

--- ubuntu20/realesrgan/install.sh
+++ ubuntu20/realesrgan/install.sh
@@ -82,7 +82,7 @@ done
 echo "=== 画像の拡大処理を実行中 ==="
 wget 'https://github.com/xinntao/Real-ESRGAN-ncnn-vulkan/blob/master/images/input2.jpg?raw=true' -O input.jpg
 xdg-open input.jpg &
-./realesrgan-ncnn-vulkan -i input -o output.jpg -s 2 -m models -n realesrgan-x4plus
+./realesrgan-ncnn-vulkan -i input.jpg -o output.jpg -s 2 -m models -n realesrgan-x4plus
 xdg-open output.jpg &
 
 echo "=== 完了しました ==="
