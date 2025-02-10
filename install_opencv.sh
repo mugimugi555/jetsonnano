76d4ad90ccdf6d227ada085d57f4c7af93caad35
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2025-02-10 13:14:42 +0900

    Update install_opencv.sh

--- install_opencv.sh
+++ install_opencv.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
+set -e
 
-# wget https://raw.githubusercontent.com/mugimugi555/jetsonnano/main/install_opencv.sh && bash install_opencv.sh ;
+# 必要なパッケージをインストール
+sudo apt update
+sudo apt install -y build-essential pkg-config git cmake \
+    libjpeg-dev libpng-dev libtiff-dev libopenexr-dev liblcms2-dev liblqr-1-0-dev \
+    libfftw3-dev libfreetype6-dev ocl-icd-opencl-dev \
+    libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev \
+    libtbb2 libtbb-dev libdc1394-22-dev libv4l-dev libxvidcore-dev libx264-dev \
+    libatlas-base-dev gfortran python3-dev
 
-#-----------------------------------------------------------------------------------------------------------------------
-# config
-#-----------------------------------------------------------------------------------------------------------------------
-OPENCV_VERSION="4-5-5" ;
+# NVIDIAの開発ツールをインストール
+sudo apt install -y nvidia-cuda-toolkit
 
-#-----------------------------------------------------------------------------------------------------------------------
-# check swap size";
-#-----------------------------------------------------------------------------------------------------------------------
-sudo echo ;
-SWAPLIMIT=`free -m | egrep '^Swap:' | tr -s ' ' | awk '{print $2}'` ;
-echo $SWAPLIMIT;
+# OpenCVのソースコードを取得
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkout 4.x
+mkdir build && cd build
 
-if [ $SWAPLIMIT -lt 6000 ] ; then
+# OpenCVのビルド
+cmake -D CMAKE_BUILD_TYPE=Release \
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+      -D WITH_CUDA=ON -D ENABLE_FAST_MATH=1 -D CUDA_FAST_MATH=1 \
+      -D WITH_CUBLAS=1 -D WITH_OPENCL=ON ..
+make -j$(nproc)
+sudo make install
+sudo ldconfig
 
-  cd ;
-  git clone https://github.com/JetsonHacksNano/installSwapfile ;
-  cd installSwapfile ;
-  ./installSwapfile.sh ;
-  # sudo reboot now ;
-
-fi
-
-#-----------------------------------------------------------------------------------------------------------------------
-# install opencv
-#-----------------------------------------------------------------------------------------------------------------------
-wget https://github.com/Qengineering/Install-OpenCV-Jetson-Nano/raw/main/OpenCV-${OPENCV_VERSION}.sh ;
-sudo chmod 755 ./OpenCV-${OPENCV_VERSION}.sh ;
-./OpenCV-${OPENCV_VERSION}.sh ;
-
-#-----------------------------------------------------------------------------------------------------------------------
-# finish
-#-----------------------------------------------------------------------------------------------------------------------
-python3 -c "import cv2; print(cv2.__version__)" ;
-
-# do examples
+# インストール確認
+echo "OpenCV installed successfully. Checking CUDA support:"
+pkg-config --modversion opencv4
