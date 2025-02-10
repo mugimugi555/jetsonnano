074660d70343647e9ff3957b27ca2075cb2ee1b2
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2025-02-10 13:13:01 +0900

    Update install_imagemagick.sh

--- install_imagemagick.sh
+++ install_imagemagick.sh
@@ -4,30 +4,11 @@ set -e
 # 必要なパッケージをインストール
 sudo apt update
 sudo apt install -y build-essential pkg-config git cmake nasm yasm \
-    libx264-dev libx265-dev libnuma-dev libvpx-dev libfdk-aac-dev \
-    libmp3lame-dev libopus-dev libass-dev libssl-dev zlib1g-dev \
-    libjpeg-dev libpng-dev libtiff-dev libopenexr-dev liblcms2-dev liblqr-1-0-dev libfftw3-dev libfreetype6-dev \
-    ocl-icd-opencl-dev
+    libjpeg-dev libpng-dev libtiff-dev libopenexr-dev liblcms2-dev liblqr-1-0-dev \
+    libfftw3-dev libfreetype6-dev ocl-icd-opencl-dev
 
 # NVIDIAの開発ツールをインストール
 sudo apt install -y nvidia-cuda-toolkit
-
-# FFmpegのソースコードを取得
-git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
-cd ffmpeg
-
-# コンフィグレーション & ビルド
-./configure --enable-nonfree --enable-gpl --enable-libx264 --enable-libx265 \
-            --enable-libvpx --enable-libfdk-aac --enable-libmp3lame --enable-libopus \
-            --enable-libass --enable-cuda --enable-cuvid --enable-nvenc \
-            --extra-cflags=-I/usr/local/cuda/include \
-            --extra-ldflags=-L/usr/local/cuda/lib64
-make -j$(nproc)
-sudo make install
-
-# インストール確認
-echo "FFmpeg installed successfully. Checking NVENC support:"
-ffmpeg -encoders | grep nvenc
 
 # ImageMagickのソースコードを取得
 git clone https://github.com/ImageMagick/ImageMagick.git
